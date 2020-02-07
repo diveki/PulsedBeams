d5fc73f934027a7fac9e449719f43f90fd9fd514
Divéki Zsolt PhD <divekizs@ELI-ALPS.local>
2020-02-07 21:01:30 +0100

    transform temporal to spectral

--- beams.py
+++ beams.py
@@ -26,16 +26,15 @@ class Light:
     def initialize_temporal_profile(self, params):
         import pdb
         # pdb.set_trace()
-        self.amplitude = params['amplitude']#, error(KeyError, '`amplitude` key and value is missing!'))
-        if type(self.amplitude) == str:
+        amplitude = params['amplitude']#, error(KeyError, '`amplitude` key and value is missing!'))
+        if type(amplitude) == str:
             print('Loading file')
-        elif type(self.amplitude) == np.ndarray:
+        elif type(amplitude) == np.ndarray:
             print('Loading numpy array')
-        elif type(self.amplitude) == float or type(self.amplitude) == int:
-            self.t = params['t']#, self.error(KeyError, '`t` key and value is missing!'))
+        elif type(amplitude) == float or type(amplitude) == int:
             self.create_beam_from_params(params)
             self.from_temporal2spectral()
-            print(f'the amplitude is {self.amplitude}')
+            print(f'the amplitude is {amplitude}')
         else:
             raise TypeError('amplitude has to be a filename or numpy array or float or integer')
 
@@ -49,40 +48,32 @@ class Light:
             self._create_beam_from_lorentzian(params)
         
     def _create_beam_from_gaussian(self, params):
+        amplitude = params['amplitude']
         sigma  = params.get('sigma', None)
         FWHM   = params.get('FWHM', None)
-        self.t0     = params.get('t0', 0)
-        self.lambda0= params['lambda0']#, self.error(KeyError, f'The key `lambda0` has to be in the argument!'))
-        self.omega0 = self._lambda2omega(self.lambda0)
-        self.polarization    = params.get('polarization', 'x')
-        self.beam = Gaussian_temporal(self.amplitude, FWHM=FWHM, spread=sigma, t0=self.t0, lambda0=self.lambda0, pol=self.polarization)
-        self.FWHM = self.beam.FWHM
-        self.spread = self.beam.param
-        self.temporal_envelope = self._calculate_temporal_envelope(self.t)
-        self.temporal_electric_field = self._calculate_temporal_electric_field(self.amplitude, self.temporal_envelope, self.omega0, self.t)
+        phase   = params.get('phase', None)
+        t0     = params.get('t0', 0)
+        t      = params.get('t')
+        lambda0= params['lambda0']#, error(KeyError, f'The key `lambda0` has to be in the argument!'))
+        polarization    = params.get('polarization', 'x')
+        self.beam = Gaussian_temporal(amp=amplitude, FWHM=FWHM, spread=sigma, t=t, t0=t0, phase=phase, lambda0=lambda0, pol=polarization)
 
     def _create_beam_from_lorentzian(self, params):
         pass
     
     def from_temporal2spectral(self):
-        self.delta_omega = self._calculate_delta_omega(self.t)
-        self.omega = self._calculate_omega(self.t, self.omega0, self.delta_omega)
-        self.lambdas = self._calculate_lambdas(self.omega)
-        self.spectrum = self._calculate_spectrum(self.temporal_electric_field)
+        self.spectral_beam.delta_omega = self._calculate_delta_omega(self.beam.t)
+        self.spectral_beam.omega = self._calculate_omega(self.beam.t, self.beam.omega0, self.spectral_beam.delta_omega)
+        self.spectral_beam.lambdas = self._calculate_lambdas(self.spectral_beam.omega)
+        self.spectral_beam.spectrum = self._calculate_spectrum(self.beam.temporal_electric_field)
+        self.spectral_beam = Spectral_Profile()
 
-    def _calculate_spectrum(self, ef):
-        return np.fft.fft(ef, norm='ortho')
+    
 
 
-    def _calculate_lambdas(self, value):
-        return lambda2omega(value)
 
-    def _calculate_omega(self, t, omega0, dw):
-        return np.fft.fftfreq(len(t))*len(t)*dw
 
-    def _calculate_delta_omega(self, t):
-        T = t.max() - t.min()
-        return 2*np.pi / T
+    
 
     def _lambda2omega(self, value):
         return 2*np.pi*sc.c / value
@@ -119,17 +110,20 @@ class Light:
 
 
 class Temporal_Envelope_Profile:
-    def __init__(self, amp=1, FWHM=None, spread = None, t=None, t0=0, phase=None, lambda0=None, pol='x'):
-        self.amplitude = amp  
-        if FWHM == None and spread == None:
-            raise ValueError(f'FWHM or spread must be a number !!')
-        self.FWHM      = FWHM 
-        self.polarization = pol
-        self.param     = spread
-        self.t         = t
-        self.t0        = t0
-        self.lambda0   = lambda0
-        self.set_params()
+    def __init__(self, amp=1, FWHM=None, spread = None, t=None, t0=0, phase=None, lambda0=None, pol='x', spectral=None):
+        if isinstance(spectral, Spectral_Profile):
+            pass
+        else:
+            self.amplitude = amp  
+            if FWHM == None and spread == None:
+                raise ValueError(f'FWHM or spread must be a number !!')
+            self.FWHM      = FWHM 
+            self.polarization = pol
+            self.param     = spread
+            self.t         = t
+            self.t0        = t0
+            self.lambda0   = lambda0
+            self.set_params()
     
     def set_params(self):
         if self.FWHM == None:
@@ -149,14 +143,13 @@ class Temporal_Envelope_Profile:
     def create_envelope(self, t):
         pass
 
-    # def _calculate_temporal_envelope(self, t):
-    #     return self.beam.create_envelope(t)
-
     def get_temporal_electric_field(self, amp, env, omega0, phase, t):
-        # import pdb
-        # pdb.set_trace()
         return amp * env * np.exp(1j*(omega0*t + phase))
     
+    def get_temporal_intensity(self, amp, env, omega0, phase, t):
+        E_t = self.get_temporal_electric_field(amp, env, omega0, phase, t)
+        return sc.epsilon_0 * sc.c * np.abs(E_t)**2
+
     def set_amplitude(self, value, update=False):
         self.amplitude = value
         if update:
@@ -190,16 +183,117 @@ class Temporal_Envelope_Profile:
         if update:
             self.update_beam()
 
+    def from_temporal2spectral(self):
+    #     self.spectral_beam.delta_omega = self._calculate_delta_omega(self.beam.t)
+    #     self.spectral_beam.omega = self._calculate_omega(self.beam.t, self.beam.omega0, self.spectral_beam.delta_omega)
+    #     self.spectral_beam.lambdas = self._calculate_lambdas(self.spectral_beam.omega)
+    #     self.spectral_beam.spectrum = self._calculate_spectrum(self.beam.temporal_electric_field)
+        return Spectral_Profile(temporal=self)
+
+
+class Spectral_Profile:
+    def __init__(self, amp=1, delta_lambda=None, spread = None, lambdas=None, phase=None, lambda0=None, pol='x', temporal=None):
+        if isinstance(temporal, Temporal_Envelope_Profile):
+            self._create_spectrum_from_temporal(temporal)
+        else:
+            self.amplitude = amp  
+            if FWHM == None and spread == None:
+                raise ValueError(f'FWHM or spread must be a number !!')
+#         self.FWHM      = FWHM 
+#         self.polarization = pol
+#         self.param     = spread
+#         self.t         = t
+#         self.t0        = t0
+#         self.lambda0   = lambda0
+#         self.set_params()
+    
+    def _create_spectrum_from_temporal(self, obj):
+        self.polarization = obj.polarization
+        self.lambda0 = obj.lambda0
+        self.omega0 = obj.omega0
+        self._create_frequencies(obj.t, self.omega0)
+        self.spectrum = self._calculate_spectrum(obj.temporal_electric_field)
+
+    def _create_frequencies(self, t, omega0):
+        self.delta_omega = get_fft_delta_omega(t)
+        self.omega = self._calculate_omega(t, self.delta_omega)
+        self.lambdas = lambda2omega(self.omega)
+    
+    def _calculate_omega(self, t, dw):
+        return np.fft.fftshift(np.fft.fftfreq(len(t)))*len(t)*dw
+
+    def _calculate_spectrum(self, ef):
+        return np.fft.fftshift(np.fft.fft(ef, norm='ortho'))
+
+  
+#     def set_params(self):
+#         if self.FWHM == None:
+#             self.FWHM = self.param2FWHM(self.param)
+#         elif self.param == None:
+#             self.param = self.FWHM2param(self.FWHM)
+
+#     def FWHM2param(self, x):
+#         pass
+
+#     def param2FWHM(self, x):
+#         pass
+    
+#     def update_beam(self):
+#         pass
+
+#     def create_envelope(self, t):
+#         pass
+
+#     # def _calculate_temporal_envelope(self, t):
+#     #     return self.beam.create_envelope(t)
+
+#     def get_temporal_electric_field(self, amp, env, omega0, phase, t):
+#         # import pdb
+#         # pdb.set_trace()
+#         return amp * env * np.exp(1j*(omega0*t + phase))
+    
+#     def set_amplitude(self, value, update=False):
+#         self.amplitude = value
+#         if update:
+#             self.update_beam()
+
+#     def set_phase(self, value, update=False):
+#         pass
+
+#     def set_FWHM(self, value, update=False):
+#         self.FWHM = value
+#         if update:
+#             self.update_beam()
+
+#     def set_spread(self, value, update=False):
+#         self.param = value
+#         if update:
+#             self.update_beam()
+
+#     def set_t0(self, value, update=False):
+#         self.t0 = value
+#         if update:
+#             self.update_beam()
+
+#     def set_lambda0(self, value, update=False):
+#         self.lambda0 = value
+#         if update:
+#             self.update_beam()
+
+#     def set_polarization(self, value, update=False):
+#         self.polarization = value
+#         if update:
+#             self.update_beam()
+
 
 class Gaussian_temporal(Temporal_Envelope_Profile):
-    def __init__(self, amp=1, FWHM=None, spread=None, lambda0=None, t=None, t0=0, phase=None, pol='x'):
-        Temporal_Envelope_Profile.__init__(self, amp=amp, FWHM=FWHM, spread=spread, t=t, t0=t0, phase=phase, lambda0=lambda0, pol=pol)
-        # import pdb
-        # pdb.set_trace()
+    def __init__(self, amp=1, FWHM=None, spread=None, lambda0=None, t=None, t0=0, phase=None, pol='x', spectral=None):
+        Temporal_Envelope_Profile.__init__(self, amp=amp, FWHM=FWHM, spread=spread, t=t, t0=t0, phase=phase, lambda0=lambda0, pol=pol, spectral=spectral)
         self.set_phase(phase)
         self.envelope = self.create_envelope(t)
         self.omega0 = lambda2omega(lambda0)
         self.temporal_electric_field = self.get_temporal_electric_field(self.amplitude, self.envelope, self.omega0, self.phase, self.t)
+        self.temporal_intensity = self.get_temporal_intensity(self.amplitude, self.envelope, self.omega0, self.phase, self.t)
     
     def FWHM2param(self, x):
         return x / (2*np.sqrt(2*np.log(2)))
@@ -216,6 +310,7 @@ class Gaussian_temporal(Temporal_Envelope_Profile):
         self.envelope = self.create_envelope(self.t)
         self.omega0 = lambda2omega(self.lambda0)
         self.temporal_electric_field = self.get_temporal_electric_field(self.amplitude, self.envelope, self.omega0, self.phase, self.t)
+        self.temporal_intensity = self.get_temporal_intensity(self.amplitude, self.envelope, self.omega0, self.phase, self.t)
             
     def set_phase(self, value, update=False):
         if value == None:
@@ -260,6 +355,8 @@ if __name__ == '__main__':
     phase = np.zeros(len(omega))
     pars = {
         't': t,
+        't0': 10e-15,
+        'phase':0,
         'amplitude':1,
         'lambda0':lambda0,
         'FWHM':30e-15,
@@ -267,3 +364,4 @@ if __name__ == '__main__':
         'name':'gaussian'
     }
     beam = Gaussian_temporal(amp=1, FWHM=30e-15, spread=None, lambda0=lambda0, t=t, t0=10e-15, phase=None, pol=polarization)
+    # beam = Light(pars, type='temporal')
